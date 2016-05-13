0aa2c317d9cd5416e88d2eeda5e7a3b21ca31c97
Li Baiming <ws02978126@yahoo.co.jp>
2016-05-13 16:42:45 +0900

    Japanese comment

--- practice1.c
+++ practice1.c
@@ -3,3 +3,10 @@
 mplementation of Dijkstra algorithm to solve the shortest path
  problem based on the techniques of dynamic memory allocation and
  recursion
+ ［最短路問題 1］
+ グラフ理論における基本的な問題の1つである最短路問題は、鉄道の乗り換え検索やカーナ
+ ビゲーション等にも応用されている。この最短路問題を解く代表的アルゴリズムであるダイ
+ クストラ法をC言語を用いて実装する。
+ 
+ */
+
